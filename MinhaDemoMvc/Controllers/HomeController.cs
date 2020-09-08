629140669a51577dc0d358b598199424e4965496
Luiz Felipe Mendes Barbosa <luiz.barbosa@radixeng.com.br>
2020-09-08 20:56:14 -0300

    First commit

---
+++ MinhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMvc.Models;
+
+namespace MinhaDemoMvc.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("")]
+        [Route("pagina-inicial")]
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        public IActionResult Privacy()
+        {
+            return Json("{'nome':'Edua'}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
